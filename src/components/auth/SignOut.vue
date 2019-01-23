64f7897d90b0b42ab63c04a90991fb296c5226f0
Fester <festersy@gmail.com>
2019-01-23 18:14:56 +0700

    SignOut: template & styles done

--- src/components/auth/SignOut.vue
+++ src/components/auth/SignOut.vue
@@ -1,14 +1,43 @@
 <template>
-    <div id='signOut'>
-        <h2>Are you sure you wanna sign out?</h2>
+    <div id='signOut' class='auth'>
+        <div class='authForm'>
+          <button class='exit' v-on:click.prevent='setCurrentTab(null)'>X</button>
+          <h2>Whoa! Are you sure you want to sign out?</h2>
+          <button id='confirmBtn' class='submitButton'>Yes, I'd like to sign out</button>
+          <button id='returnBtn' v-on:click.prevent='setCurrentTab(null)' class='submitButton'>No, I still have things to do</button>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    setCurrentTab(newTab) {
+      this.$store.dispatch('setCurrentTab', newTab);
+    }
+  }
 };
 </script>
 
 <style scoped lang='scss'>
+#confirmBtn {
+  background-color: #00dd33;
+  padding: 15px 30px;
+  width: auto;
+  height: auto;
+  font-size: 30px;
+  display: block;
+  margin: 0 auto 10px auto;
+
+  &:hover {
+    background-color: #00bb33;
+  }
+}
+
+#returnBtn {
+  padding: 10px 20px;
+  margin: 0 auto;
+  display: block;
+  width: auto;
+}
 </style>
