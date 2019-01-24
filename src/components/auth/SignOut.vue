a5d9e2f2487fe765b08dd80100ecec95202aa143
Fester <festersy@gmail.com>
2019-01-24 20:34:06 +0700

    SignOut: Sign out user when button clicked

--- src/components/auth/SignOut.vue
+++ src/components/auth/SignOut.vue
@@ -3,17 +3,25 @@
         <div class='authForm'>
           <button class='exit' v-on:click.prevent='setCurrentTab(null)'>X</button>
           <h2>Whoa! Are you sure you want to sign out?</h2>
-          <button id='confirmBtn' class='submitButton'>Yes, I'd like to sign out</button>
+          <button id='confirmBtn' class='submitButton' v-on:click='signOut'>Yes, I'd like to sign out</button>
           <button id='returnBtn' v-on:click.prevent='setCurrentTab(null)' class='submitButton'>No, I still have things to do</button>
         </div>
     </div>
 </template>
 
 <script>
+import { auth } from '../../config/firebaseConfig.js';
+
 export default {
   methods: {
     setCurrentTab(newTab) {
       this.$store.dispatch('setCurrentTab', newTab);
+    },
+    signOut() {
+      auth.signOut().then(() => {
+        console.log('User has signed out');
+        this.setCurrentTab(null);
+      });
     }
   }
 };
