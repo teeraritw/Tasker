da0e694ae6d0222d1303c961db386c7bed544c11
Fester <festersy@gmail.com>
2019-01-24 20:49:57 +0700

    SignIn: Sign in user on form submit

--- src/components/auth/SignIn.vue
+++ src/components/auth/SignIn.vue
@@ -1,18 +1,21 @@
 <template>
     <div id='signIn' class='auth'>
-        <form id='signInForm' class='authForm'>
+        <form v-on:submit.prevent='signIn' id='signInForm' class='authForm'>
             <button class='exit' v-on:click.prevent='setCurrentTab(null)'>X</button>
             <ul>
                 <li>
                     <h2 class='authHeader'>SIGN IN</h2>
+                    <transition name='fade'>
+                        <p class='errorMessage' v-if='formSubmitted.error'>{{ formSubmitted.errorMessage }}.</p>
+                    </transition>
                 </li>
                 <li>
                     <label for='emailInput'>Email:</label>
-                    <input id='emailInput' type='email' placeholder="Email" />
+                    <input v-model='user.email' id='emailInput' type='email' placeholder="Email" />
                 </li>
                 <li>
                     <label for='passwordInput'>Password:</label>
-                    <input id='passwordInput' type='password' placeholder="Password" />
+                    <input v-model='user.password' id='passwordInput' type='password' placeholder="Password" />
                 </li>
             </ul>
             <button class='submitButton' id='signInBtn'>WELCOME BACK!</button>
@@ -21,12 +24,38 @@
 </template>
 
 <script>
+import { auth } from '../../config/firebaseConfig';
+
 export default {
-  methods: {
-    setCurrentTab(newTab) {
-        this.$store.dispatch('setCurrentTab', newTab);
+    data() {
+        return {
+            user: {
+                email: '',
+                password: ''
+            },
+            formSubmitted: {
+                errorMessage: null,
+                error: false
+            }
+        };
+    },
+    methods: {
+        setCurrentTab(newTab) {
+            this.$store.dispatch('setCurrentTab', newTab);
+        },
+        signIn() {
+            auth.signInWithEmailAndPassword(this.user.email, this.user.password)
+            .then(data => {
+                console.log(data);
+                this.formSubmitted.error = false;
+                this.setCurrentTab(null);
+            }).catch(err => {
+                console.log(err);
+                this.formSubmitted.error = true;
+                this.formSubmitted.errorMessage = err.message;
+            });
+        }
     }
-  }
 };
 </script>
 
