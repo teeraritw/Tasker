78ea486451a4c91da484e93cc0fe54e390afb979
Fester <festersy@gmail.com>
2019-01-27 16:22:17 +0700

    Functions: Make sure the function is triggered once

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,20 @@ const createNotification = notification => {
     return admin.firestore().collection('notifications').add(notification);
 };
 
+var alreadyRunning = [];
+
+function isAlreadyRunning(eventId) {
+    return alreadyRunning.indexOf(eventId) >= 0;
+}
+
+function markAsRunning(eventId) {
+    alreadyRunning.push(eventId);
+}
+
 exports.projectCreated = functions.firestore.document('todos/{todosId}')
-        .onCreate(doc => {
+        .onCreate((doc, event) => {
+        let eventId = event.eventId;
+
         const todo = doc.data();
         const notification = {
             content: 'added a new todo',
@@ -16,15 +28,26 @@ exports.projectCreated = functions.firestore.document('todos/{todosId}')
             time: admin.firestore.FieldValue.serverTimestamp()
         };
 
+    if (isAlreadyRunning(eventId)) {
+        return false;
+    }
+
+    markAsRunning(eventId);
     return createNotification(notification);
 });
 
-exports.userSignedUp = functions.auth.user().onCreate(user => {
+exports.userSignedUp = functions.auth.user().onCreate((user, event) => {
+    let eventId = event.eventId;
     const notification = {
         content: 'just signed up',
         user: user.email,
         time: admin.firestore.FieldValue.serverTimestamp()
     };
 
+    if (isAlreadyRunning(eventId)) {
+        return false;
+    }
+
+    markAsRunning(eventId);
     return createNotification(notification);
 });
