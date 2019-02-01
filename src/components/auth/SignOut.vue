9647a444be1e93ca09c79bed6a5a328867c17df8
Fester <festersy@gmail.com>
2019-02-01 17:07:58 +0700

    SignOut: change styles for mobiles and default

--- src/components/auth/SignOut.vue
+++ src/components/auth/SignOut.vue
@@ -1,10 +1,9 @@
 <template>
     <div id='signOut' class='auth'>
         <div class='authForm'>
-          <button class='exit' v-on:click.prevent='setCurrentTab(null)'>X</button>
-          <h2>Whoa! Are you sure you want to sign out?</h2>
+          <h2>Are you sure you want sign out?</h2>
           <button id='confirmBtn' class='submitButton' v-on:click='signOut'>Yes, I'd like to sign out</button>
-          <button id='returnBtn' v-on:click.prevent='setCurrentTab(null)' class='submitButton'>No, I still have things to do</button>
+          <button id='returnBtn' v-on:click.prevent='setCurrentTab(null)'>No, I still have things to do..</button>
         </div>
     </div>
 </template>
@@ -27,30 +26,47 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+.authForm {
+    display: flex;
+    align-items: center;
+}
+
 #confirmBtn {
-  background-color: #00dd33;
   padding: 15px 30px;
-  width: auto;
-  height: auto;
-  font-size: 30px;
+  width: 90%;
+  height: 60px;
+  font-size: 24px;
   display: block;
   margin: 0 auto 10px auto;
-
-  &:hover {
-    background-color: #00bb33;
-  }
 }
 
 #returnBtn {
+  border: 0;
   padding: 10px 20px;
   margin: 0 auto;
   display: block;
   width: auto;
-
-  background-color: #666;
+  color: #666;
+  background: transparent;
 
   &:hover {
-    background-color: #444;
+    opacity: 0.5;
+  }
+}
+
+@media screen and (max-width: 850px) {
+  #confirmBtn {
+    font-size: 20px;
+    height: 55px;
+  }
+  h2 {
+    font-size: 22px;
+  }
+}
+
+@media screen and (max-width: 560px) {
+  h2 {
+    font-size: 20px;
   }
 }
 </style>
